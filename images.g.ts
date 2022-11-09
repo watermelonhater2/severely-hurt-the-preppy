9b0881f7afb035e7398e4c4b21685a882640dadd
watermelonhater2 <117844165+watermelonhater2@users.noreply.github.com>
2022-11-09 10:40:49 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "Vampire":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 3 3 3 3 3 3 3 . . . . . 
+. . . 3 3 3 3 3 3 3 3 3 . . . . 
+. . 3 3 3 5 3 3 3 5 3 3 3 . . . 
+. 3 3 3 3 5 3 3 3 5 3 3 3 3 . . 
+. 3 3 3 3 5 3 3 3 5 3 3 3 3 . . 
+. 3 3 3 3 5 3 3 3 5 3 3 3 3 . . 
+. 3 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+. 3 5 5 3 3 3 3 3 3 3 5 5 3 . . 
+. 3 3 5 5 3 3 3 3 3 5 5 3 3 . . 
+. 3 3 3 5 5 5 5 5 5 5 3 3 3 . . 
+. . 3 3 3 3 3 3 3 3 3 3 3 . . . 
+. . . 3 3 3 3 3 3 3 3 3 . . . . 
+. . . . 3 3 3 3 3 3 3 . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "Smiley":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f f f f f f f f f . . . . 
+. . f f f d f f f d f f f . . . 
+. f f f f d f f f d f f f f . . 
+. f f f f d f f f d f f f f . . 
+. f f f f d f f f f f f f f . . 
+. f f f f f f f f f f f f f . . 
+. f d f f f f f f f f f d f . . 
+. f f d f f f f f f f d f f . . 
+. f f f d d d d d d d f f f . . 
+. . f f f f f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
